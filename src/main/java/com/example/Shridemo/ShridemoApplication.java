12b8766e5fe63eb8fc359c15cc9e26c26905932e
Shrisirsat <shrisirsat27@gmail.com>
2024-07-24 21:45:25 +0530

    jar commit

---
+++ src/main/java/com/example/Shridemo/ShridemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Shridemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShridemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShridemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Shridemo/ShridemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Shridemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShridemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
